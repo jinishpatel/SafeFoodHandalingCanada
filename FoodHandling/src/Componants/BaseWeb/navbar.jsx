0e4ffe6b97f8f38506a1f24fa88052e807c1a47d
Jinish Patel <jinish5597@gmail.com>
2023-10-11 16:51:33 -0400

    SFHC-15 Initial navigation Bar without Media Query

--- FoodHandling/src/Componants/BaseWeb/navbar.jsx
+++ FoodHandling/src/Componants/BaseWeb/navbar.jsx
@@ -1,9 +1,24 @@
 /** @format */
 
 import React from 'react';
-
+import './navbar.css';
 const navbar = () => {
-	return <div>navbar</div>;
+	return (
+		<div className="navbar-container">
+			<div className="mainbar">
+				<div className="navbar-icon">
+					<img src="https://res.cloudinary.com/dg4btcufa/image/upload/v1697056204/Safe%20Food%20Handling%20Canada/kgop012lbfzrwl7jfzj8.png" />
+				</div>
+				<div className="navbar-list">
+					<div className="navbar-item">Home</div>
+					<div className="navbar-item">About</div>
+					<div className="navbar-item">Contact</div>
+					<div className="navbar-item">Course</div>
+					<div className="navbar-item">SignIn/Up</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default navbar;
