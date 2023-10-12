1d1ea7b16ad8c2e3648c2bf02d33ab7123c49771
Jinish Patel <jinish5597@gmail.com>
2023-10-11 22:43:00 -0400

    SFHC-16 responsive footer developed with use of media queries

--- FoodHandling/src/Componants/BaseWeb/footer.jsx
+++ FoodHandling/src/Componants/BaseWeb/footer.jsx
@@ -1,9 +1,70 @@
 /** @format */
 
 import React from 'react';
-
+import './footer.css';
 const footer = () => {
-	return <div>footer</div>;
+	return (
+		<div className="footer-container">
+			<div className="footer-left">
+				<img src="https://res.cloudinary.com/dg4btcufa/image/upload/v1697056204/Safe%20Food%20Handling%20Canada/xthijz9rfl0dvgvynkne.png" />
+			</div>
+			<div className="footer-right">
+				<div className="footer-right-left">
+					<h2>Products</h2>
+					<ul>
+						<li>
+							<a href="#"> Safety Certificate</a>
+						</li>
+						<li>
+							<a href="#">Full Course</a>
+						</li>
+						<li>
+							<a href="#">Quick Course</a>
+						</li>
+						<li>
+							<a href="#">Examination</a>
+						</li>
+					</ul>
+				</div>
+				<div className="footer-right-middle">
+					<h2>Company</h2>
+					<ul>
+						<li>
+							<a href="#"> About us</a>
+						</li>
+						<li>
+							<a href="#">Customers</a>
+						</li>
+						<li>
+							<a href="#">Events</a>
+						</li>
+						<li>
+							<a href="#">Partners</a>
+						</li>
+					</ul>
+				</div>
+				<div className="footer-right-right">
+					<h2>Contact Us</h2>
+					<ul>
+						<li>
+							<a href="#">Technical Support</a>
+						</li>
+						<li>
+							<a href="#">Education and Training</a>
+						</li>
+						<li>
+							<a href="#"></a>
+						</li>
+						<li>
+							<a href="#">Facebook |</a>
+							<a href="#"> Instagram |</a>
+							<a href="#"> Twitter</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default footer;
